05528723c943704e0865917f1511737ff60ccc45
Polymathh <wambugum110@gmail.com>
2025-04-12 20:48:59 +0300

    initial commit

--- totopal/totoStore/models.py
+++ totopal/totoStore/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -9,7 +8,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class ProductMedia(models.Model):
     MEDIA_TYPES = (
         ('image', 'Image'),
@@ -20,6 +19,8 @@ class ProductMedia(models.Model):
     media_type = models.CharField(max_length=10, choices=MEDIA_TYPES, default='image')
     image = models.ImageField(upload_to='products/images', blank=True, null=True)
     video = models.FileField(upload_to='products/videos', blank=True, null=True)
+    color = models.CharField(max_length=30, blank=True, null=True, help_text="e.g. red, blue, black")
 
     def __str__(self):
-        return f'{self.product.name} Media ({self.media_type})'
+        return f'{self.product.name} Media ({self.media_type}, {self.color})'
+
--- totopal/totoStore/views.py
+++ totopal/totoStore/views.py
@@ -3,7 +3,14 @@ from .models import Product
 
 # Create your views here.
 def home(request):
-    products = Product.objects.all()
+    products = Product.objects.prefetch_related('media').all()
+    
+    # Pre-process each product's media for unique colors
+    for product in products:
+        used_colors = []
+        for media in product.media.all():
+            if media.color and media.color not in used_colors:
+                used_colors.append(media.color)
+        product.used_colors = used_colors
+    
     return render(request, 'store/home.html', {'products': products})
-
-
